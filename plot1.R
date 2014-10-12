a234c3957f2351a4eafa722e434ba1054e3cb279
polinapopova <polina.e.popova@gmail.com>
2014-10-12 22:04:33 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 rm(list=ls())
-setwd("/Users/polunchik/Downloads")
 
-data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
