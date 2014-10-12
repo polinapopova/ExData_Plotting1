f758e3f4a1cdd997abb305a733cc914e7d0d50ff
polinapopova <polina.e.popova@gmail.com>
2014-10-12 21:56:46 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
+rm(list=ls())
+setwd("/Users/polunchik/Downloads")
+
 data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
